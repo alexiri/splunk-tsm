99c62579a46b4556e57cbbfea17f60f62c57e927
Alex Iribarren <Alex.Iribarren@cern.ch>
2017-03-13 11:18:29 +0100

    Updates to druid_admin

--- druid_admin/appserver/static/dashboard.js
+++ druid_admin/appserver/static/dashboard.js
@@ -197,14 +197,74 @@ require([
                       ]);
 
 
-    /* Editable tables */
-    var edittables = ['serverinfo_edit', 'nodeinfo_edit', 'napinfo_edit'];
-    require(['app/edittable/edittable'], function() {
-        $.each(edittables, function(i, t) {
-            if ($('#'+t).length) {
-                $(this).editTable(t);
-            }
-        });
+    function setToken(name, value) {
+        console.log('Setting Token %o=%o', name, value);
+        var defaultTokenModel = mvc.Components.get('default');
+        if (defaultTokenModel) {
+            defaultTokenModel.set(name, value);
+        }
+        var submittedTokenModel = mvc.Components.get('submitted');
+        if (submittedTokenModel) {
+            submittedTokenModel.set(name, value);
+        }
+    }
+
+    $('.dashboard-body').on('click', '[data-set-token],[data-unset-token],[data-token-json]', function(e) {
+        e.preventDefault();
+        var target = $(e.currentTarget);
+        var setTokenName = target.data('set-token');
+        if (setTokenName) {
+            setToken(setTokenName, target.data('value'));
+        }
+        var unsetTokenName = target.data('unset-token');
+        if (unsetTokenName) {
+            setToken(unsetTokenName, undefined);
+        }
+        var tokenJson = target.data('token-json');
+        if (tokenJson) {
+            try {
+                if (_.isObject(tokenJson)) {
+                    _(tokenJson).each(function(value, key) {
+                        if (value == null) {
+                            // Unset the token
+                            setToken(key, undefined);
+                        } else {
+                            setToken(key, value);
+                        }
+                     });
+                 }
+             } catch (e) {
+                 console.warn('Cannot parse token JSON: ', e);
+             }
+         }
+    });
+
+    $('.dashboard-body div[data-set-token],div[data-unset-token],div[data-token-json]').each(function() {
+        var setTokenName = $(this).data('set-token');
+        if (setTokenName) {
+            setToken(setTokenName, $(this).data('value'));
+        }
+        var unsetTokenName = $(this).data('unset-token');
+        if (unsetTokenName) {
+            setToken(unsetTokenName, undefined);
+        }
+        var tokenJson = $(this).data('token-json');
+        if (tokenJson) {
+            try {
+                if (_.isObject(tokenJson)) {
+                    _(tokenJson).each(function(value, key) {
+                        if (value == null) {
+                            // Unset the token
+                            setToken(key, undefined);
+                        } else {
+                            setToken(key, value);
+                        }
+                     });
+                 }
+             } catch (e) {
+                 console.warn('Cannot parse token JSON: ', e);
+             }
+         }
     });
 
 });
